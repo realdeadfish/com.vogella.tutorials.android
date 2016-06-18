e747b855db4a6d15033fe3988a4471c3d712d865
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-18 13:53:57 +0200

    Adds Jenkins Android to the Android tutorials (removed from Jenkins tutorial

--- AndroidBuild/res/gradle/runBuild.sh
+++ AndroidBuild/res/gradle/runBuild.sh
@@ -1,8 +1,2 @@
-# build project, runs both the assemble and check task
-gradle build
-
-# build project complete from scratch
-gradle clean build
-
 # speedup second grandle build by holding it in memory
 gradle build --daemon
