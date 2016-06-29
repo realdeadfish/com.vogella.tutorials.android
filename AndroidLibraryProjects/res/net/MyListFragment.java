5637398248f4a4f278187e103f99690b0b741548
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-29 23:41:04 +0200

    Updated Library project exercise

--- AndroidLibraryProjects/res/net/MyListFragment.java
+++ AndroidLibraryProjects/res/net/MyListFragment.java
@@ -47,8 +47,8 @@ public class MyListFragment extends Fragment {
         }
     }
 
-    // triggers update of the details fragment
-    public void updateDetail(String uri) {
+    // triggers update of the details fragment                                 
+    public void updateDetail(String uri) {  // #<1>
         List<RssItem> list = RssFeedProvider
                 .parse("http://www.vogella.com/article.rss");
         String text = String.valueOf(list.toString());
