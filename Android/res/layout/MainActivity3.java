42ccfff74cef0f8a4f136c3e19d062c8b75efea4
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-29 18:51:48 +0200

    Andriod util method to find a view easier

--- Android/res/layout/MainActivity3.java
+++
@@ -1,8 +0,0 @@
-// search in the layout of the activity
-LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mylayout);
-
-// afterwards search in linearLayout for another view
-TextView textView = (TextView) linearLayout.findViewById(R.id.mytext);
-
-// note, you could have directly searched for R.id.mytext, the above coding
-// is just for demonstration purposes
