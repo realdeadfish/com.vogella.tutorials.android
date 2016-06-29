83421c7d477bd48fec3163cbe3b1a3114aa6fe27
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-29 08:26:54 +0200

    Adjusted to support two-way databinding

--- AndroidDatabinding/res/exercise_layoutbutton/TemperatureData.java
+++ AndroidDatabinding/res/exercise_layoutbutton/TemperatureData.java
@@ -1,31 +1,31 @@
 package com.vogella.android.databinding;
 
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.ObservableField;
 
 import java.util.Observable;
 
-public class TemperatureData {
+public class TemperatureData extends BaseObservable{
     private String celsius;
 
-    public String getFahrenheit() {
-        return fahrenheit;
-    }
     public TemperatureData(String celsius) {
         this.celsius = celsius;
-    }
-    public void setFahrenheit(String fahrenheit) {
-        this.fahrenheit = fahrenheit;
     }
 
     private String fahrenheit;
 
+    @Bindable
     public String getCelsius() {
         return celsius;
     }
 
+
     public void setCelsius(String celsius) {
+
         this.celsius = celsius;
+        notifyPropertyChanged(BR.celsius);
     }
 
 
